8b4027a408d7eea03f2156b00a43d5095c5e11d4
Greg Shanks <greg.shanks@gmail.com>
2020-02-21 16:45:56 +0000

    Added INDEX route for posts

--- src/main/java/com/example/codeclan/CheckMate/controllers/UserController.java
+++ src/main/java/com/example/codeclan/CheckMate/controllers/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -20,5 +21,12 @@ public class UserController {
     public ResponseEntity<List<User>> getAllUsers() {
         return new ResponseEntity<>(userRepository.findAll(), HttpStatus.OK);
     }
+
+    @GetMapping(value="/users/{id}")
+    public ResponseEntity getUser(@PathVariable Long id) {
+        return new ResponseEntity<>(userRepository.findById(id), HttpStatus.OK);
+    }
+
+
 
 }
