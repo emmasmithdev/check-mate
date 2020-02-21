7e5370e32abe86878bd4cbb043d12f0e1c12684e
Greg Shanks <greg.shanks@gmail.com>
2020-02-21 12:43:56 +0000

    Initialised repositories

--- src/main/java/com/example/codeclan/CheckMate/repositories/PostRepository.java
+++ src/main/java/com/example/codeclan/CheckMate/repositories/PostRepository.java
@@ -1,4 +1,9 @@
 package com.example.codeclan.CheckMate.repositories;
 
-public interface PostRepository {
+import com.example.codeclan.CheckMate.models.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
 }
