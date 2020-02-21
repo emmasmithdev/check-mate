bd6c01c3f0af3775a3f9ebf21d6a93fd2f7e48cb
Greg Shanks <greg.shanks@gmail.com>
2020-02-21 17:28:24 +0000

    CRUD functionality added in routes for User

--- src/main/java/com/example/codeclan/CheckMate/controllers/UserController.java
+++ src/main/java/com/example/codeclan/CheckMate/controllers/UserController.java
@@ -5,9 +5,7 @@ import com.example.codeclan.CheckMate.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -27,6 +25,23 @@ public class UserController {
         return new ResponseEntity<>(userRepository.findById(id), HttpStatus.OK);
     }
 
+    @PostMapping(value="/users")
+    public ResponseEntity<User> postUser(@RequestBody User user) {
+        userRepository.save(user);
+        return new ResponseEntity<>(user, HttpStatus.CREATED);
+    }
 
+    @PatchMapping(value="/users/{id}")
+    public ResponseEntity<User> updateUser(@RequestBody User user) {
+        userRepository.save(user);
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+
+    @DeleteMapping(value="/users/{id}")
+    public ResponseEntity<User> deleteUser(@PathVariable Long id) {
+        User found = userRepository.getOne(id);
+        userRepository.delete(found);
+        return new ResponseEntity<>(null, HttpStatus.OK);
+    }
 
 }
