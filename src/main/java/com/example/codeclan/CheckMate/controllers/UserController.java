86492f3fe0f7222b08b38b9f2fc70b0d21561022
Greg Shanks <greg.shanks@gmail.com>
2020-02-21 16:20:09 +0000

    Added INDEX route for users

--- src/main/java/com/example/codeclan/CheckMate/controllers/UserController.java
+++ src/main/java/com/example/codeclan/CheckMate/controllers/UserController.java
@@ -1,4 +1,24 @@
 package com.example.codeclan.CheckMate.controllers;
 
+import com.example.codeclan.CheckMate.models.User;
+import com.example.codeclan.CheckMate.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class UserController {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @GetMapping(value="/users")
+    public ResponseEntity<List<User>> getAllUsers() {
+        return new ResponseEntity<>(userRepository.findAll(), HttpStatus.OK);
+    }
+
 }
