ee1d5ef15d621f626d0f1af87ca9e706d436c0b7
Emma <emmagray183@gmail.com>
2020-02-21 13:36:41 +0000

    userRepository

--- src/main/java/com/example/codeclan/CheckMate/repositories/UserRepository.java
+++ src/main/java/com/example/codeclan/CheckMate/repositories/UserRepository.java
@@ -1,4 +1,7 @@
 package com.example.codeclan.CheckMate.repositories;
 
-public interface UserRepository {
+import com.example.codeclan.CheckMate.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
 }
